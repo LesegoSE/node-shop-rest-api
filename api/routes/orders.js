30fe3794085f57c7b959fc4592625b7a75fcd412
LesegoSE <lesego.seikali@gmail.com>
2025-07-20 10:02:12 +0200

    adding code to extract data from the body of orders & products POST requests

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -26,8 +26,13 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.post('/',(req,res,next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(200).json({
-        message: 'Order has been succesfully created'
+        message: 'Order has been succesfully created',
+        order: order
     });
 });
 
