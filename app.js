6cf06dc41c753b2789286ba0d6724226d4fec57c
LesegoSE <lesego.seikali@gmail.com>
2025-07-20 08:13:47 +0200

    adding middleware for orders api

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const productRouter = require('./api/routes/product');
+const orderRouter = require('./api/routes/orders');
 
 app.use('/products', productRouter);
+app.use('/orders', orderRouter);
 
 module.exports = app;
