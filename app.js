9efb0a7aa8919051e23ecb0b6f002a127567d0dd
LesegoSE <lesego.seikali@gmail.com>
2025-07-21 22:34:53 +0200

    adding connection string to MongoDB database

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRouter = require('./api/routes/product');
 const orderRouter = require('./api/routes/orders');
 
+/*mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_NODE_SHOP_PW + '@node-shop.nqvytnp.mongodb.net/?retryWrites=true&w=majority&appName=node-shop',
+    {
+        useMongoClient : true
+    }
+);//connection string to MongoDB database
+*/
+
+mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_NODE_SHOP_PW + '@node-shop.nqvytnp.mongodb.net/?retryWrites=true&w=majority&appName=node-shop');
+
 app.use(morgan('dev')); //Handles request logging
-app.use(bodyParser.urlencoded({extended: false})); //Handles body data
+app.use(bodyParser.urlencoded({ extended: false })); //Handles body data
 app.use(bodyParser.json()); //Extracts json data in a easy to read format
 
 //Prevents CORS errors
-app.use((req,res,next) => {
-    res.header('Acces-Control-Allow-Origin','*');
+app.use((req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
@@ -26,14 +36,14 @@ app.use('/products', productRouter);
 app.use('/orders', orderRouter);
 
 //Handles all errors that occure with the api's
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
     next(error);
 });
 
 //Handles all errors that occure at any instance
-app.use((error,req,res,next) => {
+app.use((error, req, res, next) => {
     res.status(error.status || 500)
     res.json({
         error: {
