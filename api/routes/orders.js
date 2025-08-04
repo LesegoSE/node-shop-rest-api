0f6e15887817f14a52b0618405be95d5db22a5ad
LesegoSE <lesego.seikali@gmail.com>
2025-08-04 06:58:59 +0200

    adding code to post orders to the database

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
 
 //Handling requests
 router.get('/', (req, res, next) => {
@@ -24,20 +27,32 @@ router.get('/:orderId', (req, res, next) => {
     }
 });
 
-router.post('/',(req,res,next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(200).json({
-        message: 'Order has been succesfully created',
-        order: order
+router.post('/', (req, res, next) => {
+    const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        quantity: req.body.quantity,
+        product: req.body.productId
     });
+    order
+        .save()
+        //.exec() //turns it to a true promise
+        .then(result => {
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+
+        });
+   
 });
 
-router.delete('/:orderId', (req,res,next) => {
+router.delete('/:orderId', (req, res, next) => {
     const id = req.params.orderId
-    
+
     res.status(200).json({
         message: 'Order has been succesfully deleted',
         id: id
